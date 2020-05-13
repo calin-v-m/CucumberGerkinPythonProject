6413c5845984e03b9f70775239c772923fa7933e
CalinVM <calinlandshark@yahoo.com>
2020-05-13 18:15:45 +0200

    changed name to driver, before browser

--- tests/acceptance/steps/content.py
+++ tests/acceptance/steps/content.py
@@ -6,12 +6,12 @@ use_step_matcher('re')
 
 @given('I am on the homepage')
 def step_impl(context):
-    context.browser = webdriver.Chrome()
-    context.browser.get('https://www.seleniumeasy.com/')
-    context.browser.maximize_window()
+    context.driver = webdriver.Chrome()
+    context.driver.get('https://www.seleniumeasy.com/')
+    context.driver.maximize_window()
 
 
 @then('The title has the text "(.*)"')
 def step_impl(context, content):
-    title_tag = context.browser.find_element_by_css_selector('#site-name h1').text
-    assert title_tag == content
+    title_tag = context.driver.find_element_by_css_selector('#site-name h1')
+    assert title_tag.text == content
--- tests/acceptance/steps/navigation.py
+++ tests/acceptance/steps/navigation.py
@@ -6,29 +6,29 @@ use_step_matcher('re')
 
 @given('I am on the homepage of Selenium Easy')
 def step_impl(context):
-    context.browser = webdriver.Chrome()
-    context.browser.get('https://www.seleniumeasy.com/')
-    context.browser.maximize_window()
+    context.driver = webdriver.Chrome()
+    context.driver.get('https://www.seleniumeasy.com/')
+    context.driver.maximize_window()
 
 
 @given('I am on the homepage that has a search option')
 def step_impl(context):
-    context.browser = webdriver.Chrome()
-    context.browser.get('https://www.seleniumeasy.com/')
-    context.browser.maximize_window()
+    context.driver = webdriver.Chrome()
+    context.driver.get('https://www.seleniumeasy.com/')
+    context.driver.maximize_window()
 
 
 @then('I am on the Maven page')
 def step_impl(context):
     expected_url = 'https://www.seleniumeasy.com/maven-tutorials'
-    print(context.browser.current_url)
+    print(context.driver.current_url)
     print(expected_url)
-    assert context.browser.current_url == expected_url
+    assert context.driver.current_url == expected_url
 
 
 @then('it shows all the results that include the word searched')
 def step_impl(context):
     expected_url = 'https://www.seleniumeasy.com/search/node/JUnit'
-    print(context.browser.current_url)
+    print(context.driver.current_url)
     print(expected_url)
-    assert context.browser.current_url == expected_url
+    assert context.driver.current_url == expected_url
